bdc8c69795611d0a70148a768d2b5520be12ef8d
MindlessGamingDE <157584320+MindlessGamingDE@users.noreply.github.com>
2024-03-20 08:46:57 +0100

    edited

--- gameserver/minecraft_installer.sh
+++ gameserver/minecraft_installer.sh
@@ -1,5 +1,5 @@
-echo "---------------------------------------"
+echo "╔═══━━━━━━━━━━━━━━━━━━━━─── ◥◣◆◢◤ ───━━━━━━━━━━━━━━━━━━━━━━━━═══╗"
 echo "Dieses Installations-script wurde von @MindlessGamingDE erstellt! Mehr Infos auf meinen Discord:"
-echo " This installation-script was created by @MindlessGamingDE! More information on my Discord:"
-echo "---------------------------------------"
+echo "This installation-script was created by @MindlessGamingDE! More information on my Discord:"
+echo "╚══━━━━━━━━━━━━━━━━━━━━━─── ◥◣◆◢◤ ───━━━━━━━━━━━━━━━━━━━━━━━━━━═╝"
 wait 10
